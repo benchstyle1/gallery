3d643b31ffce95faa05e5b5239a4dd44e3d255db
benchstyle1 <47250962+canijke@users.noreply.github.com>
2020-07-15 14:48:31 +0300

    deploy

---
+++ precache-manifest.da5efd4c18f2038ddbd54dafb1232dd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98dc8661236387470277",
+    "url": "/gallery/css/chunk-0fea9be3.b53e607f.css"
+  },
+  {
+    "revision": "d95d6d567650f7b612aa66c01f4c1133",
+    "url": "/gallery/index.html"
+  },
+  {
+    "revision": "6e048f7f4d4098166f36",
+    "url": "/gallery/js/app.db4cede3.js"
+  },
+  {
+    "revision": "98dc8661236387470277",
+    "url": "/gallery/js/chunk-0fea9be3.5ddd5f29.js"
+  },
+  {
+    "revision": "d85cc683b8f65b4e2790",
+    "url": "/gallery/js/chunk-vendors.bdf58acf.js"
+  },
+  {
+    "revision": "4e73d18b30d4e3c3ff835d275ed7a33f",
+    "url": "/gallery/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/gallery/robots.txt"
+  }
+]);
